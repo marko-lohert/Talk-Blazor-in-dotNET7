121464a08fdb70667d62ba7b2b9a0be940c1b135
Marko Lohert <lohert.marko@gmail.com>
2022-10-23 22:07:29 +0200

    Cancel navigation if user has begun entering data in this page.

--- Demo/EmployeeDirectory/EmployeeDirectory/Client/Pages/NewEmployee.razor.cs
+++ Demo/EmployeeDirectory/EmployeeDirectory/Client/Pages/NewEmployee.razor.cs
@@ -1,4 +1,5 @@
 ﻿using EmployeeDirectory.Shared;
+using Microsoft.AspNetCore.Components.Routing;
 using System.Net.Http.Json;
 
 namespace EmployeeDirectory.Client.Pages;
@@ -6,6 +7,10 @@ namespace EmployeeDirectory.Client.Pages;
 public partial class NewEmployee
 {
     public Employee NewEmployeeModel { get; set; } = new();
+
+    // Did user start entered any data in this page?
+    public bool IsDirty { get; set; } = false;
+
     protected override void OnInitialized()
     {
         base.OnInitialized();
@@ -15,9 +20,24 @@ public partial class NewEmployee
         // Default date of employment is today.
         NewEmployeeModel.DateOfEmployment = DateTime.Now.Date;
     }
-    
+
     private void NewUserSubmit()
     {
         Http.PostAsJsonAsync<Employee>("Employee", NewEmployeeModel);
+    }
+
+    void OnDataInput()
+    {
+        IsDirty = true;
+    }
+
+    async Task BeforeNavigationAsync(LocationChangingContext context)
+    {
+        bool userConfirmed = await JS.InvokeAsync<bool>("window.confirm", new object[] { "If you leave this page, you'll lose your changes." });
+
+        if (userConfirmed is false)
+        {
+            context.PreventNavigation();
+        }
     }
 }
