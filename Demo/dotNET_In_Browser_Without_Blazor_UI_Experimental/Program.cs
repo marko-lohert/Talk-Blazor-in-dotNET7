e24774d08b5f28d6950375b75c8f71529c2749d1
Marko Lohert <lohert.marko@gmail.com>
2022-10-27 09:56:00 +0200

    Multithreading_Experimental (experimental feature from the wasm-experimental workload).

--- Demo/dotNET_In_Browser_Without_Blazor_UI_Experimental/Program.cs
+++ Demo/dotNET_In_Browser_Without_Blazor_UI_Experimental/Program.cs
@@ -1,7 +1,40 @@
 using System;
 using System.Runtime.InteropServices.JavaScript;
+using System.Threading;
 
 Console.WriteLine("Hello, Browser!");
+
+// Multithreading
+
+const int minSleepPeriod = 100; // ms
+const int maxSleepPeriod = 3000; // ms
+
+// Run multiple threads in .NET app running in a browser (using WASM).
+new Thread(SecondThread).Start();
+MainThread();
+
+static void MainThread()
+{
+    Console.WriteLine($"Main thread: ManagedThreadId =  {Thread.CurrentThread.ManagedThreadId}");
+
+    for (int i = 0; i < 10; ++i)
+    {
+        Console.WriteLine($"Main thread: step {i}");
+        Thread.Sleep(Random.Shared.Next(minSleepPeriod, maxSleepPeriod));
+    }
+}
+
+static void SecondThread()
+{
+    Console.WriteLine($"Second thread: ManagedThreadId = {Thread.CurrentThread.ManagedThreadId}");
+
+    for (int i = 0; i < 10; ++i)
+    {
+        Console.WriteLine($"Second thread: step {i}");
+        Thread.Sleep(Random.Shared.Next(minSleepPeriod, maxSleepPeriod));
+    }
+}
+
 
 public partial class MyClass
 {
